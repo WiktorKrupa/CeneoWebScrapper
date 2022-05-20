b688487f16834d0b802e41eb7ab046e93780fd88
WiktorKrupa <s221422@student.uek.krakow.pl>
2022-05-20 15:33:59 +0200

    WK 05.20

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index/<name>')
+def index(name="<<Hello World>>"):
+    return render_template("index.html", text = name)
+
+
